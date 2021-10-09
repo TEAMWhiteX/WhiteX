3fbc90412803dab405f9a38e22c6c04331db9201
TEAMWhiteX <80911568+TEAMWhiteX@users.noreply.github.com>
2021-10-09 20:41:09 +0300

    Add files via upload

--- sultan.lua
+++ sultan.lua
@@ -8133,7 +8133,7 @@ keyboard.inline_keyboard = {
 {{text = 'Channel Source ،',url="t.me/UUi9U"}},
 }
 local msg_id = msg.id_/2097152/0.5 
-https.request("https://api.telegram.org/bot"..token..'/sendVoice?chat_id=' .. msg.chat_id_ .. '&voice=https://t.me/AudiosDavid/'..sultan..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+https.request("https://api.telegram.org/bot"..token..'/sendVoice?chat_id=' .. msg.chat_id_ .. '&voice=https://t.me/AudiosDavid/'..sultansong..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 --     Source sultan     --
 if text == "تفعيل ريمكس" and Owner(msg)  or text == "تفعيل الريمكس" and Owner(msg)  then
